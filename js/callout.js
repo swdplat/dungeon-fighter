831a9b005037aa1ec93be77fc5de2b1f2322aed7
lu193740 <103547656+lu193740@users.noreply.github.com>
2026-09-03 16:27:54 +0800

    0903樣式修改

---
+++ js/callout.js
@@ -0,0 +1,46 @@
+// JavaScript Document
+$(document).ready(function() {
+	
+	$("#login_callout").hide()
+	$("#callout").hide()
+	$(".nav_name").hide()
+	$(".nav_team").hide()
+	
+    $("#how").click(function(){
+	  $("#callout").show()
+	});	
+	
+	$("#callout_close").click(function(){
+	  $("#callout").hide()
+	});	
+	
+	$("#how4").click(function(){
+	  $("#callout").show()
+	});	
+	
+	$("#how3").click(function(){
+	  $("#callout").show()
+	});	
+	
+        $("#how1").click(function(){
+	  $("#callout").show()
+	});
+	
+	$("#nav_login_btn").click(function(){
+	  $("#nav_login_btn").hide()
+	  $("#login_callout").show()
+	});	
+	
+	$("#login_callout_close").click(function(){
+        $("#nav_login_btn").show()
+		$("#login_callout").hide()
+	});	
+	
+	$("#id_submit").click(function(){
+	  $("#login_callout").hide()
+	  $("#nav_login_btn").hide()
+	  $(".nav_team").show()
+	  $(".nav_name").show()
+	});	
+
+});
--- js/tab.js
+++ js/tab.js
@@ -11,9 +11,15 @@ $(document).ready(function() {
 	$("#server").hide()
 	$("#character").hide()
 	$(".btn_confirm").hide()
-	$(".btn_search2").hide()
+	$("#btn_search1").hide()
+	$("#btn_search2").hide()
+	$("#btn_research1").hide()
 	$("#search_team").hide()
 	$("#search_team2").hide()
+	$(".form1").hide()
+	$(".form2").hide()
+    $(".tb1").hide()
+	$(".tb3").hide()
 	
 	
 	$(".tab2").click(function(){
@@ -24,7 +30,8 @@ $(document).ready(function() {
 	  $(".tab2").hide()
 	  $(".tab2r").show()
       $(".form1").hide()
-	  $(".form2").show()
+	  $(".form2").hide()
+      $("#search_result_space").show()
 	});	
 	
 	$(".tab1").click(function(){
@@ -34,45 +41,77 @@ $(document).ready(function() {
 	  $(".tab1r").show()
 	  $(".tab2r").hide()
 	  $(".tab2").show()
-      $(".form2").hide()
-	  $(".form1").show()
+      $(".form1").hide()
+	  $(".form2").hide()
+      $("#search_result_space").show()
 	});	
 	
+
 	
-	
-    $("#how").click(function(){
-	  $("#callout").show()
-	});	
-	
-	$("#callout_close").click(function(){
-	  $("#callout").hide()
-	});	
-	
-	$("#id_submit").click(function(){
-	  $("#server").show()
-	  $("#character").show()
-	  $(".btn_confirm").show()
-      $("#member_ID").hide()
-      $("#how").hide()
-	});	
-	
-	$("#btn_search1").click(function(){
-      $("#search_ID").hide()
-      $("#how").hide()
-	  $("#btn_search1").hide()
-	  $("#btn_research1").show()
+	$("#btn_submit1").click(function(){
+	  $("#btn_submit1").hide()
+	  $("#btn_search1").show()
       $("#search_team").show()
 	});	
 	
-	$("#btn_search2").click(function(){
-      $("#search_ID2").hide()
-	  $("#search_mycard").hide()
-      $("#how2").hide()
-	  $("#btn_search2").hide()
-	  $("#btn_research2").show()
+	$("#btn_search1").click(function(){
+	  $(".form1").show()
+	  $("#btn_search1").hide()
+	  $("#btn_research1").show()
+      $("#search_result_space").hide()
+	});	
+	
+	$("#btn_submit2").click(function(){
+      $(".form2").hide()
+	  $("#btn_submit2").hide()
+	  $("#btn_search2").show()
       $("#search_team2").show()
 	});	
 	
+	$("#btn_search2").click(function(){
+      $(".form2").show()
+	  $("#btn_submit2").hide()
+	  $("#btn_search2").show()
+      $("#search_team2").show()
+	  $("#search_result_space").hide()
+	});	
 	
+
+	$("#tresure_tab1").click(function(){
+	  $(".tb1").show()
+	  $(".tb2").hide()
+      $(".tb3").hide()
+	  $("#tresure_tab1").addClass("on")
+      $("#tresure_tab1").removeClass("off")
+      $("#tresure_tab2").addClass("off")
+      $("#tresure_tab2").removeClass("on")
+	  $("#tresure_tab3").addClass("off")
+      $("#tresure_tab3").removeClass("on")
+	});	
+	
+	$("#tresure_tab2").click(function(){
+	  $(".tb2").show()
+	  $(".tb1").hide()
+      $(".tb3").hide()
+	  $("#tresure_tab1").addClass("off")
+      $("#tresure_tab1").removeClass("on")
+      $("#tresure_tab2").addClass("on")
+      $("#tresure_tab2").removeClass("off")
+	  $("#tresure_tab3").addClass("off")
+      $("#tresure_tab3").removeClass("on")
+	});	
+	
+	$("#tresure_tab3").click(function(){
+	  $(".tb3").show()
+	  $(".tb1").hide()
+      $(".tb2").hide()
+	  $("#tresure_tab1").addClass("off")
+      $("#tresure_tab1").removeClass("on")
+      $("#tresure_tab2").addClass("off")
+      $("#tresure_tab2").removeClass("on")
+	  $("#tresure_tab3").addClass("on")
+      $("#tresure_tab3").removeClass("off")
+	});	
+
 
 });
